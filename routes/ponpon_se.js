017fef45cabd2e491d16361e9ad717ac95f8fef3
izawasoma <izawasoma@gmail.com>
2022-03-26 20:30:41 +0900

    ルーティングを修正

--- routes/ponpon_se.js
+++ routes/ponpon_se.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/ponpon_se', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('ponpon_se', { title: 'Socket.io + Express' });
 });
 
