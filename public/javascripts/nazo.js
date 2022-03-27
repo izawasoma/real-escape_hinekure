5cc1adefa011abbb1bc41b1f2254a214251defc2
Yuki Wada <75063370+Wada810@users.noreply.github.com>
2022-03-27 20:38:20 +0900

    細部の修正

--- public/javascripts/nazo.js
+++ public/javascripts/nazo.js
@@ -1,3 +1,4 @@
+/////////////謎の答えなどの定数定義、および初期値を変数に格納////////////////
 const ans_list = {
     q1: {
         answer: "518",
@@ -39,17 +40,35 @@ let state = {
     //どこまで正解したか
     progress : 0
 }
+//進展に応じた初期値を設定
+for(let i = 0; i <= state.progress; i++){
+    console.log(i);
+    $('.p_nav__item').eq(i).removeClass("--disabled");
+}
+if(state.progress == 7){
+    nav(state.progress);
+}else{
+    nav(state.progress + 1);
+}
 
+/////////////////////イベント設定///////////////////
 //解答する
 $('#answer_button').on("click",()=>{
     const q_num = $('.p_nav__item.--active').text();
     check_ans(q_num);
 })
+//ナビおよび問題の切り替え
+$('.p_nav__item').on("click",(e)=>{
+    const q_num = e.target.textContent;
+    nav(q_num);
+})
+
+/////////////////////////関数////////////////////////////
 //正誤判定
 function check_ans(q_num){
     //特殊問題
     if(q_num == 3){
-        if($('.p_content__input').eq(0).val() == "UL" && $('.p_content__input').eq(1).val() == "OOL" && $('.p_content__input').eq(2).val() == "495"){
+        if($('.p_content__input').eq(0).val() == "UL" && $('.p_content__input').eq(1).val() == "00L" && $('.p_content__input').eq(2).val() == "495"){
             correct(q_num);
         }else{
             incorrect();
@@ -64,6 +83,7 @@ function check_ans(q_num){
         }
     }
 }
+//正解時の処理
 function correct(q_num){
     correct_modal();
     $('.p_content__info').removeClass("--disabled");
@@ -75,6 +95,113 @@ function correct(q_num){
 }
 function incorrect(){
     $('.p_content__incorrect').removeClass('hidden');
+}
+//ナビの切り替え
+function nav(q_num){
+    $('.q3').remove();
+    $('.q4').remove();
+    $('.q7').remove();
+    $('.p_content__form').removeClass('hidden');
+    $('.p_content__nazo_image').removeClass('hidden');
+    if(q_num == 3){
+        $('.p_content__nazo_image').attr("src",`img/question/3-1.png`);
+        $('.p_content__nazo_container').append(`<img class="p_content__nazo_image q3" src="img/question/3-2.png">`);
+        $('.p_content__nazo_container').append(`<img class="p_content__nazo_image q3" src="img/question/3-3.png">`);
+        //input
+        $('.p_content__input').after(`<input class="c_input--text p_content__input q3" type="text" name="">`)
+        $('.p_content__input').eq(0).after(`<input class="c_input--text p_content__input q3" type="text" name="">`)
+    }else if(q_num == 4){
+        $('.p_content__form').addClass('hidden');
+        $('.p_content__nazo_image').addClass('hidden');
+        q4();
+    }else if(q_num == 7){
+        $('.p_content__nazo_image').attr("src",`img/question/7-1.png`);
+        $('.p_content__nazo_container').append(`<div class="p_modal--star__open q7"><span class="material-icons">collections</span>星を見る</div>`);
+        q7modal();
+    }else{
+        $('.p_content__nazo_image').attr("src",`img/question/${q_num}.png`);
+    }
+    $('.p_content__question_text').text(ans_list["q" + q_num].question);
+    $('.p_nav__item.--active').removeClass("--active");
+    $('.p_nav__item').eq(q_num - 1).addClass("--active");
+    $('.p_content__incorrect').addClass('hidden');
+
+    if(state.progress >= q_num){
+        if(q_num == 3){
+            $('.p_content__input').eq(0).val("UL");
+            $('.p_content__input').eq(1).val("OOL");
+            $('.p_content__input').eq(2).val("495");
+        }else if(q_num == 4){
+            $('.p_content__q4box').addClass("correct");
+            $('.p_content__col').eq(4).addClass("selected");
+            $('.p_content__col').eq(0).addClass("selected");
+            $('.p_content__col').eq(20).addClass("selected");
+            $('.p_content__col').eq(12).addClass("selected");
+            $('.p_content__col').eq(24).addClass("selected");
+            $('.p_content__col').eq(6).addClass("selected");
+            $('.p_content__col').eq(8).addClass("selected");
+            $('.p_content__col').eq(16).addClass("selected");
+        }
+        else{
+            $('.p_content__input').val(ans_list["q" + q_num].answer);
+        }
+        $('.p_content__info').removeClass("--disabled")
+    }else{
+        $('.p_content__input').val("");
+        $('.p_content__info').addClass("--disabled")
+    }
+}
+//q4盤面生成
+function q4(){
+    //マス目生成
+    $('.p_content__nazo_container').append(`<div class="p_content__q4box q4"></div>`)
+    for(let i = 0; i < 5; i ++){
+        $('.p_content__q4box').append(`<div class="p_content__row q4"></div>`)
+        for(let j = 0; j < 5; j ++){
+            $('.p_content__row').eq(i).append(`<div class="p_content__col q4"></div>`)
+        }
+    }
+    $('.p_content__col').height($('.p_content__col').width());
+    let que = [];
+    $('.p_content__col').on("click",(e)=>{
+        //console.log($('.p_content__col').index(e.currentTarget));
+        $(e.currentTarget).addClass("selected");
+        //正誤判定
+        que.push($('.p_content__col').index(e.currentTarget))
+        if(que.length < 8){
+            console.log(que);
+        }else{
+            if(JSON.stringify(que) == JSON.stringify(q4ans)) {
+                correct(4);
+                $('.p_content__q4box').addClass("correct");
+            }else{
+                $('.p_content__q4box').on('animationend',()=>{
+                    que = [];
+                    $('.p_content__col').removeClass('selected');
+                    $('.p_content__q4box').removeClass("incorrect");
+                })
+                $('.p_content__q4box').addClass("incorrect");
+            }
+        }
+    })
+
+}
+/////////////モーダル//////////////////
+//q7 modal
+function q7modal(){
+    $('.p_modal--star__open').click(()=>{
+        $('.p_modal--star').addClass("shown");
+    });
+    $('.p_modal--star').click((e)=>{
+        $('.p_modal--star').addClass("close");
+        $('.p_modal--star').one('animationend',()=>{
+            $('.p_modal--star').removeClass("shown");
+            $('.p_modal--star').removeClass("close");
+        })
+    });
+    $(".p_modal--info__image").click((e)=>{
+        e.stopPropagation();
+    })
 }
 //正解モーダル
 function correct_modal(){
@@ -98,85 +225,6 @@ $('.p_modal--info').click((e)=>{
         $('.p_modal--info').removeClass("close");
     })
 });
-$(".p_modal--info__image").click((e)=>{
+$(".p_modal--info__content").click((e)=>{
     e.stopPropagation();
 })
-//ナビおよび問題の切り替え
-$('.p_nav__item').on("click",(e)=>{
-    const q_num = e.target.textContent;
-    $('.q3').remove();
-    $('.q4').remove();
-    $('.q7').remove();
-    $('.p_content__form').removeClass('hidden');
-    $('.p_content__nazo_image').removeClass('hidden');
-    if(q_num == 3){
-        $('.p_content__nazo_image').attr("src",`img/question/3-1.png`);
-        $('.p_content__nazo_container').append(`<img class="p_content__nazo_image q3" src="img/question/3-2.png">`);
-        $('.p_content__nazo_container').append(`<img class="p_content__nazo_image q3" src="img/question/3-3.png">`);
-        //input
-        $('.p_content__input').after(`<input class="c_input--text p_content__input q3" type="text" name="">`)
-        $('.p_content__input').eq(0).after(`<input class="c_input--text p_content__input q3" type="text" name="">`)
-    }else if(q_num == 4){
-        $('.p_content__form').addClass('hidden');
-        $('.p_content__nazo_image').addClass('hidden');
-        q4();
-    }else if(q_num == 7){
-        $('.p_content__nazo_image').attr("src",`img/question/7-1.png`);
-        $('.p_content__nazo_container').append(`<img class="p_content__nazo_image q7" src="img/question/7-2.png">`);
-    }else{
-        $('.p_content__nazo_image').attr("src",`img/question/${q_num}.png`);
-    }
-    $('.p_content__question_text').text(ans_list["q" + q_num].question);
-    $('.p_nav__item.--active').removeClass("--active");
-    $(e.target).addClass("--active");
-    $('.p_content__incorrect').addClass('hidden');
-
-    if(state.progress >= q_num){
-        if(q_num == 3){
-            $('.p_content__input').eq(0).val("UL");
-            $('.p_content__input').eq(1).val("OOL");
-            $('.p_content__input').eq(2).val("495");
-        }else{
-            $('.p_content__input').val(ans_list["q" + q_num].answer);
-        }
-        $('.p_content__info').removeClass("--disabled")
-    }else{
-        $('.p_content__input').val("");
-        $('.p_content__info').addClass("--disabled")
-    }
-})
-
-//q4盤面生成
-function q4(){
-    //マス目生成
-    $('.p_content__nazo_container').append(`<div class="p_content__q4box q4"></div>`)
-    for(let i = 0; i < 5; i ++){
-        $('.p_content__q4box').append(`<div class="p_content__row q4"></div>`)
-        for(let j = 0; j < 5; j ++){
-            $('.p_content__row').eq(i).append(`<div class="p_content__col q4"></div>`)
-        }
-    }
-    $('.p_content__col').height($('.p_content__col').width());
-    let que = [];
-    $('.p_content__col').on("click",(e)=>{
-        //console.log($('.p_content__col').index(e.currentTarget));
-        $(e.currentTarget).addClass("selected");
-        //正誤判定
-        que.push($('.p_content__col').index(e.currentTarget))
-        if(que.length < 8){
-            console.log(que);
-        }else{
-            if(JSON.stringify(que) == JSON.stringify(q4ans)) {
-                correct(4);
-            }else{
-                $('.p_content__q4box').on('animationend',()=>{
-                    que = [];
-                    $('.p_content__col').removeClass('selected');
-                    $('.p_content__q4box').removeClass("incorrect");
-                })
-                $('.p_content__q4box').addClass("incorrect");
-            }
-        }
-    })
-
-}
